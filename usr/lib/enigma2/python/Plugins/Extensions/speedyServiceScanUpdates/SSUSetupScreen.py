9ee42982d749db01b4b187d9df7b39b198ce776a
speedy005 <67871007+speedy005@users.noreply.github.com>
2025-08-28 07:44:35 +0200

    Update SSUSetupScreen.py

--- usr/lib/enigma2/python/Plugins/Extensions/speedyServiceScanUpdates/SSUSetupScreen.py
+++ usr/lib/enigma2/python/Plugins/Extensions/speedyServiceScanUpdates/SSUSetupScreen.py
@@ -5,6 +5,7 @@ import zipfile
 import shutil
 import traceback
 import time
+import gettext
 
 try:
     import requests
@@ -22,6 +23,22 @@ for base in (
         plugin_path = possible
         break
 
+# ===== Lokalisierung für .mo Dateien =====
+locale_dir = os.path.join(plugin_path, "locale") if plugin_path else None
+lang = "en"
+try:
+    import locale
+    lang = locale.getdefaultlocale()[0]
+except Exception:
+    pass
+
+if locale_dir and os.path.isdir(locale_dir):
+    gettext.bindtextdomain("speedyservicescanupdates", locale_dir)
+    gettext.textdomain("speedyservicescanupdates")
+    _ = gettext.gettext
+else:
+    _ = lambda s: s  # Fallback: keine Übersetzung
+
 # --- Enigma2 imports ---
 from enigma import ePixmap, eLabel, getDesktop, eTimer
 from Screens.Screen import Screen
@@ -33,8 +50,6 @@ from Components.config import config, ConfigSubsection, ConfigYesNo, getConfigLi
 from Plugins.Plugin import PluginDescriptor
 from Tools.Directories import resolveFilename, SCOPE_CONFIG
 from Components.Button import Button
-# Local translations
-from . import _
 
 # ===== Version =====
 def read_version():
@@ -98,7 +113,7 @@ class SSUSetupScreen(ConfigListScreen, Screen):
                 <ePixmap pixmap="skin_default/div-h.png" position="10,650" zPosition="2" size="1180,2" />
                 <ePixmap pixmap="skin_default/buttons/yellow.png" position="630,7" size="5,70" scale="stretch" alphatest="on" />
                 <widget name="key_yellow" position="638,6" size="300,70" font="Regular;30" halign="center" valign="center" foregroundColor="yellow" />
-                <widget name="version" position="444,593" size="150,50" font="Regular;30" valign="center" halign="left" />
+                <widget name="version" position="1040,600" size="150,50" font="Regular;30" valign="center" halign="left" />
                 <ePixmap pixmap="skin_default/buttons/blue.png" position="945,7" size="5,70" scale="stretch" alphatest="on" />
                 <widget name="key_blue" position="954,8" size="250,70" font="Regular;30" halign="center" valign="center" foregroundColor="blue" />
                 <widget name="help" position="10,655" size="1180,140" font="Regular;32" />
@@ -116,8 +131,10 @@ class SSUSetupScreen(ConfigListScreen, Screen):
                 <widget name="key_blue" position="618,1" size="200,40" font="Regular;22" halign="center" valign="center" foregroundColor="blue" />
                 <widget name="config" position="5,50" itemHeight="30" size="900,390" enableWrapAround="1" scrollbarMode="showOnDemand" />
                 <ePixmap pixmap="skin_default/div-h.png" position="0,445" zPosition="2" size="900,2" />
-                <widget name="version" position="5,450" size="200,30" font="Regular;22" valign="center" halign="left" />
-                <widget name="help" position="210,450" size="685,65" font="Regular;22" />
+                <widget name="version" position="708,408" size="200,30" font="Regular;22" valign="center" halign="left" zPosition="1" />
+                <widget name="help" position="7,450" size="888,65" font="Regular;22" />
+<eLabel text="HELP" position="828,6" size="80,35" backgroundColor="#777777" valign="center" halign="center" font="Regular;24" zPosition="5" />
+<ePixmap pixmap="skin_default/buttons/vkey_exit.png" position="857,482" size="35,25" scale="stretch" alphatest="on" zPosition="6" />
             </screen>"""
 
         # --- Widgets ---
@@ -128,7 +145,7 @@ class SSUSetupScreen(ConfigListScreen, Screen):
         self["key_blue"] = Button(_("Update"))
         self["help"] = Label(_("Configure the update options."))
 
-        # --- Aktionen (inkl. EPG/Help-Taste) ---
+        # --- Aktionen ---
         self['actions'] = ActionMap(
             ['ColorActions', 'OkCancelActions', 'HelpActions', 'EPGSelectActions'],
             {
@@ -144,7 +161,6 @@ class SSUSetupScreen(ConfigListScreen, Screen):
             -1
         )
 
-        # ConfigList initialisieren
         self.onLayoutFinish.append(self.populateList)
         self["config"].onSelectionChanged.append(self.updateHelp)
 
@@ -170,7 +186,6 @@ class SSUSetupScreen(ConfigListScreen, Screen):
         for entry in self.list:
             entry[1].helpText = entry[2]
         self["config"].list = self.list
-        
 
     def restore_default(self):
         self.session.open(MessageBox, _("Default settings restored!"), MessageBox.TYPE_INFO, 3)
@@ -193,17 +208,14 @@ class SSUSetupScreen(ConfigListScreen, Screen):
         self.session.open(MessageBox, _("Configure the update options."), MessageBox.TYPE_INFO, 5)
 
     def updateHelp(self):
-        # Bei ConfigList-Auswahl die Hilfe aktualisieren
         if self["config"].getCurrent():
             self["help"].setText(self["config"].getCurrent()[1].helpText)
-
 
     def checkUpdate(self):
         if SSUUpdateScreen:
             self.session.open(SSUUpdateScreen, show_only=True)
         else:
             _safe_msg(self.session, _("Update screen not available."), MessageBox.TYPE_ERROR, 5)
-
 
 # ===== ServiceScan Hooks / Autostart =====
 _base_execBegin = None
@@ -221,7 +233,6 @@ def _has(d, k):
     except Exception:
         return False
 
-# Hooks bleiben größtenteils gleich, nur Python 3 kompatibel
 def ServiceScan_execBegin_hook(self, *args, **kwargs):
     global _preScanDB
     if SSULameDBParser and not _preScanDB:
